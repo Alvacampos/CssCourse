262c07c45e6a672211928500ad7e0160c0a281a2
Gonzalo Alvarez Campos <gonzaloralvarezcampos@gmail.com>
2019-01-16 09:41:05 -0300

    fix backdrop and sidedraw menu interaction

--- shared.js
+++ shared.js
@@ -2,15 +2,35 @@ let backdrop = document.querySelector('.backdrop');
 let modal = document.querySelector('.modal');
 let modalButtonNo = document.querySelector('.modal__action--negative');
 let buttons = document.querySelectorAll('.button');
+let toggleButton = document.querySelector('.toggle-button');
+let mobileNav = document.querySelector('.mobile-nav');
 
 buttons.forEach(element => {
   element.addEventListener('click', () => {
-    backdrop.style.display = "block";
-    modal.style.display = "block";
+    openModal();
   })
 });
 
 modalButtonNo.addEventListener('click', () => {
+  closeModal();
+});
+
+toggleButton.addEventListener('click', () => {
+  mobileNav.style.display = 'block';
+  backdrop.style.display = 'block';
+});
+
+backdrop.addEventListener('click', () => {
+  mobileNav.style.display = 'none';
+  closeModal();
+});
+
+let openModal = () => {
+  backdrop.style.display = "block";
+  modal.style.display = "block";
+};
+
+let closeModal = () => {
   backdrop.style.display = "none";
   modal.style.display = "none";
-})
+};
